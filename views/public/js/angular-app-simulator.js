ecc9c2898c243ff181d6aa248433a9c3404dc5db
Travis DePuy <tdepuy@xmatters.com>
2017-12-29 11:57:21 -0800

    Added success message and fixed drop down

--- views/public/js/angular-app-simulator.js
+++ views/public/js/angular-app-simulator.js
@@ -1,7 +1,7 @@
 // Angular App
 var octoapp = angular.module( 'octoapp', [ "ngMessages", "ngTagsInput", "xm-service" ] );
 
-octoapp.controller( 'octoSimController', function( $scope, $http, $timeout, XMService ){
+octoapp.controller( 'octoSimController', function( $scope, $http, $timeout, $q, $filter, XMService ){
 
 
    $scope.allApps = [];
@@ -19,32 +19,56 @@ octoapp.controller( 'octoSimController', function( $scope, $http, $timeout, XMSe
 
 
   $scope.selectedRecipients = [];
-  $scope.selectedApp;
+  $scope.selectedApp; // = "-- Select --"; 
+  $scope.is_finished = false;
 
-
+ 
 
    $scope.getApplications = function( query ) {
+      var deferred = $q.defer();
+      deferred.resolve( $filter('filter')($scope.allApps, query) );
+      return deferred.promise;
 
-      return $scope.allApps;
+   }
+
+   $scope.selectApp = ( app ) => {
+      $scope.selectedApp = app;
    }
 
 
-
 	$scope.runSimulation = () => {
+
+		if( !$scope.selectedApp ) {
+			console.log( 'Select an application!' );
+			$scope.simulatorForm.$invalid = true;
+			return;
+		}
+
+		if( $scope.simulatorForm.$invalid ) {
+			console.log( "Form is invaild. Check entries and try again" );
+			return;
+		}
+
 
 		var recipients = $scope.selectedRecipients.map( item => { return item.targetName } );
 		
 		var postData = { 
 			"recipients": recipients, 
-			"application": $scope.selectedApp 
+			"application": $scope.selectedApp[0].text 
 		};
 
 		$http.post( '/simulator/', postData )
 		.then( function() { 
 			console.log( 'Successful post!' );
+			$scope.is_finished = true;
+			$scope.theMessageHeader = "Success!";
+			$scope.theMessage = "Successfully sent simulation";
 
 		}, function() {
 			console.log( 'Error posting!' );
+			$scope.is_finished = true;
+			$scope.theMessageHeader = "Error!";
+			$scope.theMessage = "Errors happened. Check console and logs!";
 
 		}); // error
 
